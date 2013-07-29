57964dba6074464e22f5b801ce8a4420ac3ed516
Chiqui <chiquiesteban@gmail.com>
2013-07-29 12:38:13 -0400

    Update code.js
    Fixed bugs with Firefox

--- code.js
+++ code.js
@@ -1,10 +1,41 @@
 var thissvg;
 var highlighted = ["WA", "MA", "DC", "VA"];
+var isFirefox = typeof InstallTrigger !== 'undefined';
 
 $(document).ready(function(){
 
-  $("#svg").height($("#svg").width()*0.618)
+if(isFirefox == true) {
+	console.log("Firefox");
+	var mysvg = document.getElementById("svg");
+	var mysvgw = mysvg.getAttributeNS(null,'width')
+	mysvg.getAttributeNS('height', mysvgw*0.618);
 
+	  $(".state").mouseover( function(evt){
+	    	thissvg = evt.target;
+			$(thissvg)
+				.css("cursor","pointer")
+				.css("opacity",".5")
+				.css("filter","alpha(opacity=50%)");
+		});
+	  $(".state").mouseout( function(evt){
+			thissvg = evt.target;
+			$(thissvg)
+				.css("opacity","1")
+				.css("filter","alpha(opacity=100%)");
+		});
+
+	  $(window).resize(function(){
+	  		mysvgw = mysvg.getAttributeNS(null,'width')
+			mysvg.getAttributeNS('height', mysvgw*0.618);
+		})
+
+	  for (var i = 0; i < highlighted.length; i++) {
+			$("#"+highlighted[i]).css("fill","#9B1518");
+	   };
+
+} else {
+	console.log("NoFirefox");
+	$("#svg").height($("#svg").width()*0.618)
     $(".state").mouseover( function(){
     	thissvg = $(event.target).parent();
 		$(thissvg)
@@ -26,5 +57,6 @@ $(document).ready(function(){
 	for (var i = 0; i < highlighted.length; i++) {
 		$("#"+highlighted[i]).css("fill","#9B1518");
 	};
+}
 
 });
